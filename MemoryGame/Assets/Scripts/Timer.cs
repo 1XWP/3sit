d0c59367c7f47baf2a00a7354dd153e213d34f2b
KatarzynaOrdon <katarzynaordon@gmail.com>
2016-03-23 22:43:48 +0100

    timer added

---
+++ MemoryGame/Assets/Scripts/Timer.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+public class Timer : MonoBehaviour
+{
+    public float interval;
+
+    public GameScript gameScript;
+
+    void Awake()
+    {
+        gameScript = GetComponent<GameScript>();
+    }
+
+    public void Update()
+    {
+        if (gameScript.arrayCardsFlipped.Count == 2)
+        {
+            if (interval > 0)
+            {
+                interval -= Time.deltaTime;
+            }
+            if (interval <= 0)
+            {
+                gameScript.arrayCardsFlipped.ForEach(gameScript.SetDown);
+                gameScript.arrayCardsFlipped = new List<GameScript.Card>();
+                interval = 2;
+            }
+        }
+    }
+}
